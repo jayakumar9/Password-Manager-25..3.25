3f1f5cc932bd5cb230acb26c0b74e24cbc3ab79c
jayakumar9 <jayakumarveeran09@gmail.com>
2025-03-25 17:11:50 +0530

    Set up frontend and backend with CORS and environment variables

--- server/index.js
+++ server/index.js
@@ -8,10 +8,15 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', // Frontend URL
+  credentials: true
+}));
 
 // MongoDB Atlas Connection
 const mongoURI = process.env.MONGO_URI;
+
+//console.log('MONGO_URI:', process.env.MONGO_URI);
 
 mongoose.connect(mongoURI)
     .then(() => console.log('MongoDB Connected'))
